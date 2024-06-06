478cbc5c00a57880ca39a4723e3942cf43fbdb65
husickova <156668585+husickova@users.noreply.github.com>
2024-06-06 12:57:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import os
 
 # Run the add_ga.py script
-os.system('python add_ga.py')
+result = os.system('python add_ga.py')
+if result != 0:
+    print("Error occurred while running add_ga.py")
+    exit(1)
 
 # Run the Streamlit app
-os.system('streamlit run app.py')
+result = os.system('streamlit run app.py')
+if result != 0:
+    print("Error occurred while running the Streamlit app")
+    exit(1)
