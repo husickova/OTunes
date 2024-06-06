d8a5e3760251f22af8415ae5680fa458e3143ace
husickova <156668585+husickova@users.noreply.github.com>
2024-06-06 19:55:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import datetime
 
 # CSS styles to center the text and logo
 st.markdown(
@@ -29,47 +30,29 @@ st.markdown('<p class="center-text">Choose your favorite channel:</p>', unsafe_a
 # Selection for genres
 genre = st.selectbox('Channels:', ('Choose', 'OTunes POP', 'OTunes ROCK', 'OTunes HIPHOP', 'OTunes ELECTRO', 'OTunes COUNTRY'))
 
-if genre == 'OTunes POP':
-    # Embed YouTube Music Player for Pop
-    pop_playlist_url = "https://www.youtube.com/embed?listType=playlist&list=PLatjrwfoBSuxxjxuA4VqoDPhe_bWEGSJ-&autoplay=1"
-    pop_playlist_embed_code = f'''
-    <iframe width="100%" height="380" src="{pop_playlist_url}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+# Function to calculate time offset
+def calculate_time_offset():
+    now = datetime.datetime.now()
+    seconds_since_midnight = (now - now.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds()
+    return int(seconds_since_midnight)
+
+# Calculate offset
+time_offset = calculate_time_offset()
+
+# Dictionary of playlist URLs
+playlists = {
+    'OTunes POP': "PLatjrwfoBSuxxjxuA4VqoDPhe_bWEGSJ-",
+    'OTunes ROCK': "PLatjrwfoBSuxGIzdXo07_4-SAe-ZltkNE",
+    'OTunes ELECTRO': "PLatjrwfoBSuz9XAw-X-y5EsF-O62ZrAIf",
+    'OTunes HIPHOP': "PLatjrwfoBSuz-zbfooyidyyiwooJWfSG4",
+    'OTunes COUNTRY': "PLatjrwfoBSuzQjOKCrPtE6Vbo3rdF1TsZ"
+}
+
+# Embed YouTube Music Player based on genre and offset
+if genre in playlists:
+    playlist_url = f"https://www.youtube.com/embed?listType=playlist&list={playlists[genre]}&start={time_offset}&autoplay=1"
+    playlist_embed_code = f'''
+    <iframe width="100%" height="380" src="{playlist_url}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     '''
 
-    st.markdown(pop_playlist_embed_code, unsafe_allow_html=True)
-
-elif genre == 'OTunes ROCK':
-    # Embed YouTube Music Player for Rock
-    rock_playlist_url = "https://www.youtube.com/embed?listType=playlist&list=PLatjrwfoBSuxGIzdXo07_4-SAe-ZltkNE&autoplay=1"
-    rock_playlist_embed_code = f'''
-    <iframe width="100%" height="380" src="{rock_playlist_url}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    '''
-
-    st.markdown(rock_playlist_embed_code, unsafe_allow_html=True)
-
-elif genre == 'OTunes ELECTRO':
-    # Embed YouTube Music Player for Electro
-    electro_playlist_url = "https://www.youtube.com/embed?listType=playlist&list=PLatjrwfoBSuz9XAw-X-y5EsF-O62ZrAIf&autoplay=1"
-    electro_playlist_embed_code = f'''
-    <iframe width="100%" height="380" src="{electro_playlist_url}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    '''
-
-    st.markdown(electro_playlist_embed_code, unsafe_allow_html=True)
-
-elif genre == 'OTunes HIPHOP':
-    # Embed YouTube Music Player for HipHop
-    hiphop_playlist_url = "https://www.youtube.com/embed?listType=playlist&list=PLatjrwfoBSuz-zbfooyidyyiwooJWfSG4&autoplay=1"
-    hiphop_playlist_embed_code = f'''
-    <iframe width="100%" height="380" src="{hiphop_playlist_url}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    '''
-
-    st.markdown(hiphop_playlist_embed_code, unsafe_allow_html=True)
-
-elif genre == 'OTunes COUNTRY':
-    # Embed YouTube Music Player for Country
-    country_playlist_url = "https://www.youtube.com/embed?listType=playlist&list=PLatjrwfoBSuzQjOKCrPtE6Vbo3rdF1TsZ&autoplay=1"
-    country_playlist_embed_code = f'''
-    <iframe width="100%" height="380" src="{country_playlist_url}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    '''
-
-    st.markdown(country_playlist_embed_code, unsafe_allow_html=True)
+    st.markdown(playlist_embed_code, unsafe_allow_html=True)
