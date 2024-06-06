e00d76422f071a66c5d2f2246c2dc3883133e2d4
husickova <156668585+husickova@users.noreply.github.com>
2024-06-06 13:10:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 from bs4 import BeautifulSoup
 import pathlib
 import shutil
-import subprocess
+import streamlit as st
 
 # Google Analytics ID
 GA_ID = "google_analytics"
@@ -20,12 +20,12 @@ GA_SCRIPT = """
 def inject_ga():
     index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
     soup = BeautifulSoup(index_path.read_text(), features="html.parser")
-    if not soup.find(id=GA_ID): 
+    if not soup.find(id=GA_ID):
         bck_index = index_path.with_suffix('.bck')
         if bck_index.exists():
-            shutil.copy(bck_index, index_path)  
+            shutil.copy(bck_index, index_path)
         else:
-            shutil.copy(index_path, bck_index)  
+            shutil.copy(index_path, bck_index)
         html = str(soup)
         new_html = html.replace('<head>', '<head>\n' + GA_SCRIPT)
         index_path.write_text(new_html)
