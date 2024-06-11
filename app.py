7ca3785bdab587c6f3d5d99163adb70fa433099c
husickova <156668585+husickova@users.noreply.github.com>
2024-06-11 21:18:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,15 @@ import streamlit.components.v1 as components
 # Set the page configuration
 st.set_page_config(page_title="OTUNES", page_icon="🎵", layout="centered")
 
-# Add the local Plausible script using components.html to ensure it's added to the document head
+# Add the Plausible script using components.html to ensure it's added to the document head
 components.html(
     """
     <script>
     document.addEventListener('DOMContentLoaded', function() {
         var plausibleScript = document.createElement('script');
         plausibleScript.defer = true;
-        plausibleScript.src = 'plausible.js';
+        plausibleScript.setAttribute('data-domain', 'otunes.streamlit.app');
+        plausibleScript.src = 'https://plausible.io/js/script.js';
         plausibleScript.onload = function() {
             console.log('Plausible script loaded successfully');
         };
@@ -122,51 +123,4 @@ with streamlit_analytics.track():
     # Dictionary of playlist IDs
     playlists = {
         'OTUNES POP': "PLatjrwfoBSuyTmoyyTTIXZUU1ZpF5dBBR",
-        'OTUNES ROCK': "PLatjrwfoBSuynTvtU_VMg24LnfZ7Q2icD",
-        'OTUNES ELECTRO': "PLatjrwfoBSuxp0YTB4n77ApNaq-xJpvYl",
-        'OTUNES HIPHOP': "PLatjrwfoBSuwufuOh0Ofi3KnpEwRq8i2i",
-        'OTUNES COUNTRY': "PLatjrwfoBSuwbFst7WVhR3XYC1bsw65hx"
-    }
-
-    # Get current hour
-    current_hour = datetime.datetime.now().hour
-
-    # Determine the starting video index based on the current hour
-    if 0 <= current_hour < 3:
-        video_index = 0
-    elif 3 <= current_hour < 5:
-        video_index = 29
-    elif 5 <= current_hour < 7:
-        video_index = 49
-    elif 7 <= current_hour < 9:
-        video_index = 69
-    elif 9 <= current_hour < 11:
-        video_index = 89
-    elif 11 <= current_hour < 13:
-        video_index = 99
-    elif 13 <= current_hour < 15:
-        video_index = 109
-    elif 15 <= current_hour < 17:
-        video_index = 119
-    elif 17 <= current_hour < 19:
-        video_index = 139
-    elif 19 <= current_hour < 21:
-        video_index = 149
-    elif 21 <= currenthour < 23:
-        video_index = 159
-    elif 23 <= current_hour < 24:
-        video_index = 169
-    else:
-        video_index = 0
-
-    # Embed YouTube Music Player based on selected genre
-    if genre in playlists:
-        playlist_id = playlists[genre]
-        video_url = f"https://www.youtube.com/embed?listType=playlist&list={playlist_id}&index={video_index}&autoplay=1"
-        video_embed_code = f'''
-        <iframe width="100%" height="380" src="{video_url}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        '''
-
-        st.markdown(video_embed_code, unsafe_allow_html=True)
-
-    st.markdown('<p class="center-text">SCHOOL PROJECT AT DAB/VŠE PRAGUE FOR TV ÓČKO</p>', unsafe_allow_html=True)
+        'OTUNES ROCK': "PLatjrwfoBSuynTvtU_VMg24Lnf
