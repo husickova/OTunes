f77edc3ae5408cce26d70810ac897043542626fd
husickova <156668585+husickova@users.noreply.github.com>
2024-06-11 13:17:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,13 @@ def main():
     # Load Google Analytics script from an external file
     GA_SCRIPT = """
     <head>
+    <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-16H3MEHP7P"></script>
     <script>
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
+
       gtag('config', 'G-16H3MEHP7P');
     </script>
     </head>
